cefc256f36f64593e2ce291269a0a2e5a717ec32
handlename <nagata@handlena.me>
2014-09-14 12:46:42 +0900

    impl open-repository

--- open-github.el
+++ open-github.el
@@ -1,8 +1,30 @@
 ;;; package --- Open URL of Github for current file
 ;;; Commentary:
 ;;; Code:
+(defun open-github:chomp (str)
+  (replace-regexp-in-string "[\n\r]+$" "" str))
+
+(defun open-github:get-remote-repository-url ()
+  (if (buffer-file-name)
+      (progn
+        (cd (file-name-directory (buffer-file-name)))
+        (let*
+            ((remote-raw (shell-command-to-string "git config --get remote.origin.url"))
+             (remote     (open-github:chomp remote-raw)))
+          (if (string-match "^[^@]+@\\([^:]+\\):\\([^/]+\\)/\\([^\\.]+\\)\\.git$" remote)
+              (let
+                  ((host (match-string 1 remote))
+                   (user (match-string 2 remote))
+                   (repo (match-string 3 remote)))
+                (format "https://%s/%s/%s" host user repo)))))))
+
 (defun open-github:open-repository ()
-  "Open repository top page on Github for current file.")
+  "Open repository top page on Github for current file."
+  (interactive)
+  (let ((repo-url (open-github:get-remote-repository-url)))
+    (if repo-url
+        (browse-url repo-url)
+      (message "It seems not a file on github."))))
 
 (defun open-github:open-file ()
   "Open file page on Github for current file.
