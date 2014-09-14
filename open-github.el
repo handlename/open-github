0f6d32973e139bc3135e70d38a7318cf1b6218d5
handlename <nagata@handlena.me>
2014-09-14 13:23:10 +0900

    impl open-history

--- open-github.el
+++ open-github.el
@@ -18,6 +18,18 @@
                    (repo (match-string 3 remote)))
                 (format "https://%s/%s/%s" host user repo)))))))
 
+(defun open-github:get-branch ()
+  (let* ((symbolic-ref-raw (shell-command-to-string "git symbolic-ref HEAD"))
+         (symbolic-ref     (open-github:chomp symbolic-ref-raw)))
+    (string-match "^refs/heads/\\(.+\\)$" symbolic-ref)
+    (match-string 1 symbolic-ref)))
+
+(defun open-github:get-file-path ()
+  (let* ((top-path-raw (shell-command-to-string "git rev-parse --show-toplevel"))
+         (top-path     (open-github:chomp top-path-raw))
+         (abs-path     (buffer-file-name)))
+    (replace-regexp-in-string (format "^%s/" top-path) "" abs-path)))
+
 (defun open-github:open-repository ()
   "Open repository top page on Github for current file."
   (interactive)
@@ -39,7 +51,14 @@ If region is selected, open with line parameters.")
 If region is selected, open with line parameters.")
 
 (defun open-github:open-history ()
-  "Open history page on Github for current file.")
+  "Open history page on Github for current file."
+  (interactive)
+  (let ((repo-url (open-github:get-remote-repository-url)))
+    (if repo-url
+        (let ((branch    (open-github:get-branch))
+              (file-path (open-github:get-file-path)))
+          (browse-url (format "%s/commits/%s/%s" repo-url branch file-path)))
+      (message "It seems not a file on github."))))
 
 (defun open-github:open-pull-request ()
   "Open pull-request top page on Github for current file.")
